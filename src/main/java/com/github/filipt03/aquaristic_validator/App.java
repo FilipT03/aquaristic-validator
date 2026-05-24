1df7d587d991e752d35c4f82af50143808699972
FilipT03 <filipsombor@gmail.com>
2026-05-25 01:26:33 +0200

    Add simple rules and sample app

--- src/main/java/com/github/filipt03/aquaristic_validator/App.java
+++ src/main/java/com/github/filipt03/aquaristic_validator/App.java
@@ -1,13 +1,55 @@
 package com.github.filipt03.aquaristic_validator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.github.filipt03.aquaristic_validator.model.domain.Aquarium;
+import com.github.filipt03.aquaristic_validator.model.domain.Fish;
+import com.github.filipt03.aquaristic_validator.model.inference.Penalty;
+import com.github.filipt03.aquaristic_validator.model.types.SubstrateType;
+
+import java.util.Collection;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class App {
+    public static void main(String[] args) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession("aquariumKSession");
+
+        Aquarium tank = new Aquarium(60.0, 5, SubstrateType.SHARP_GRAVEL);
+        
+        Fish kuhliLoach = new Fish(
+            "Pangio semicincta", 
+            "Cobitidae", 
+            "benthopelagic", 
+            10.0, 
+            8
+        );
+
+        kSession.insert(tank);
+        kSession.insert(kuhliLoach);
+
+        kSession.fireAllRules();
+
+        Collection<Penalty> penalties = kSession
+            .getObjects(obj -> obj instanceof Penalty)
+            .stream()
+            .map(obj -> (Penalty)obj)
+            .toList();
+
+        int score = 100;
+
+        for (Penalty p : penalties) {
+            score -= p.getValue();
+        }
+
+        System.out.println("Final Score: " + score);
+
+        for (Penalty p : penalties) {
+            System.out.println("\t" + p.getMessage());
+        }
+        
+        kSession.dispose();
     }
 }
---
+++ src/main/java/com/github/filipt03/aquaristic_validator/model/types/HazardType.java
@@ -0,0 +1,8 @@
+package com.github.filipt03.aquaristic_validator.model.types;
+
+public enum HazardType {
+    PHYSICAL_INJURY,
+    WATER_QUALITY,
+    OVERCROWDING
+}
+    
