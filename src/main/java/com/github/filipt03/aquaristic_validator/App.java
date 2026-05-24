7b459bd3a490fe346fe04e4af54fd74dc9c8c33a
FilipT03 <filipsombor@gmail.com>
2026-05-24 22:38:30 +0200

    Initialize maven project

---
+++ src/main/java/com/github/filipt03/aquaristic_validator/App.java
@@ -0,0 +1,13 @@
+package com.github.filipt03.aquaristic_validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
